fd24b9083fa7d904eef20168591827369a8cb1d4
Manoj Muthina <manoj.muthina@continuserve.com>
2021-09-30 08:50:02 +0530

    Added code to publish component to NPM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-component-test';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+     */}
+     <Button label="test" kind="primary"/>
     </div>
   );
 }
